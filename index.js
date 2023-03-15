63a663ba5b0395dca4bfcd12e7d85cb6f284c7f8
Al Rafi <60866655+Rafi1999@users.noreply.github.com>
2023-03-15 22:06:01 +0600

    done2

--- index.js
+++ index.js
@@ -46,21 +46,19 @@ startQuiz.addEventListener("click", () => {
 // All quiz data fetched from json
 const loadQuiz = async () => {
   const res = await fetch("./data/quiz.json");
-  const data = await res.json;
-  console.log(data[0]);
+  const data = await res.json();
   quizData = data;
-  displayQuiz(data);
+  displayQuiz(quizData);
 };
 
 // Displaying quiz on quiz page
 const displayQuiz = (data) => {
   if (!data) {
-    quizContainer.innerHTML = "";
+    quizContainer.innerHTML = '';
     return;
   }
   
   data.forEach((quiz, i) => {
-    
     quizContainer.innerHTML += `<div class="m-3 py-3 px-4 shadow-sm rounded">
   <div class="flex items-center">
     <div class="h-8 w-8 bg-green-300 rounded-full flex justify-center items-center text-green-800 mr-3">
@@ -72,6 +70,7 @@ const displayQuiz = (data) => {
     ${displayQuizOptions(quiz.options, i)}
   </div>
 </div>`;
+
   });
 };
 
